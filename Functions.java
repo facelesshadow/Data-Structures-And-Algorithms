c1f9abc5888d8136d287a815652a57d6d4d9eb76
facelesshadow <aryanyadavsh369@gmail.com>
2023-10-26 13:40:43 +0530

    Added New Files

---
+++ Functions.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Functions {
+    public static void main(String[] args) {
+        int bruh = sum();
+        System.out.println("bruh is ra" + bruh);
+    }    
+
+    static int sum(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the numbers");
+        int num1 = in.nextInt();
+        int num2 = in.nextInt();
+
+        int x = num1 + num2;
+        return x;
+    }
+
+}
