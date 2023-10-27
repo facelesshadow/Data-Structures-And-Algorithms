d16a4b83e7656548e7a1170990a4f6ab2b5acbb6
facelesshadow <aryanyadavsh369@gmail.com>
2023-10-27 23:16:24 +0530

    Updated Functions.java

--- Functions.java
+++ Functions.java
@@ -2,18 +2,29 @@ import java.util.Scanner;
 
 public class Functions {
     public static void main(String[] args) {
-        int bruh = sum();
-        System.out.println("sum is " + bruh);
-    }    
-
-    static int sum(){
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter the numbers");
-        int num1 = in.nextInt();
-        int num2 = in.nextInt();
+        int a = 10;
+        int b = 20;
+        swap(a, b);
+        System.out.println("A is "+ a);
+        System.out.println("B is " + b);
+        
+     }
 
-        int x = num1 + num2;
-        return x;
-    }
-
+     static void swap(int a, int b)
+     {
+        int temp = a;
+        a = b;
+        b = temp;
+     }
+    //     int bruh = sum();
+    //     
+    // }    
+    // static int sum(){
+    //     Scanner in = new Scanner(System.in);
+    //     System.out.println("Enter the numbers");
+    //     int num1 = in.nextInt();
+    //     int num2 = in.nextInt();
+    //     int x = num1 + num2;
+    //     return x;    
 }
