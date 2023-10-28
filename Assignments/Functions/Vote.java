e53cad9ea7cf43fdf11fe6439011fd1c1445b5f5
facelesshadow <aryanyadavsh369@gmail.com>
2023-10-28 15:21:18 +0530

    Added New File

---
+++ Assignments/Functions/Vote.java
@@ -0,0 +1,21 @@
+package Assignments.Functions;
+import java.util.Scanner;
+public class Vote {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter the Age:");
+        int age = in.nextInt();
+        Vote_check(age);
+    }
+
+    static void Vote_check(int age){
+        if (age>=18){
+            System.out.println("The persi=on is eligible to vote");
+        }
+        else{
+            System.out.println("The person is not eligible to vote");
+        }
+    }
+    
+}
