820dfec53394e00961c665f64d4b92a9091d4e85
facelesshadow <aryanyadavsh369@gmail.com>
2023-11-09 00:29:02 +0530

    Added New Files

---
+++ Array1.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class Array1 {
+    public static void main(String[] args) {
+    int[] num2 = {1, 2, 3, 4};
+    System.out.println(Arrays.toString(num2));
+    }
+}
--- Arrays.java
+++ Arrays.java
@@ -1,7 +1,6 @@
-import java.util.Scanner;
+import java.util.*;
 public class Arrays {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
 
         //Array Declaration
         int[] num = new int[5];
@@ -16,5 +15,14 @@ public class Arrays {
          String[] arr = new String[4];
 
          System.out.println(arr[3]);
+
+         //Enhanced for loop
+         for(int n : num2){
+            System.out.print(n + " ");
+         }
+
+         //Printing array like an arrat by converting it into a string by Array class
+         //System.out.println(Arrays.toString(num2));
+
     }
 }
