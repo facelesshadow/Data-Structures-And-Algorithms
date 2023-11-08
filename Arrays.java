9b2cea93b0a0a7891e8247601a66ff25e8fbb6b7
facelesshadow <aryanyadavsh369@gmail.com>
2023-11-08 21:20:49 +0530

    Added Arrays.java

---
+++ Arrays.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Arrays {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        //Array Declaration
+        int[] num = new int[5];
+        //or
+        int[] num2 = {1, 2, 3 ,4 ,5};
+        //or
+        int[] num3;
+        num3 = new int[5];
+
+        System.out.println(num3[2]);
+
+         String[] arr = new String[4];
+
+         System.out.println(arr[3]);
+    }
+}
