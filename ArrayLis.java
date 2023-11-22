f826113a09193643b896e99fef23352cf6b13937
facelesshadow <aryanyadavsh369@gmail.com>
2023-11-22 22:18:21 +0530

    Added Files with ArrayLists

---
+++ ArrayLis.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList; 
+import java.util.Scanner;
+
+public class ArrayLis {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        ArrayList<Integer> name = new ArrayList<>(1);
+        name.add(69);
+        name.add(369);
+        name.add(39);
+        name.add(36);
+        name.add(3229);
+        name.add(29);
+
+        System.out.println(name);
+    }    
+}
