ea77911d8677439b09f8e354105eaa46ab13c14e
facelesshadow <aryanyadavsh369@gmail.com>
2023-11-22 23:36:09 +0530

    Added MultiAL.java

---
+++ MultiAL.java
@@ -0,0 +1,9 @@
+public class MultiAL {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        ArrayList<ArrayList<Integer>> name = new ArrayList<>();
+        
+    }
+    
+}
