4051295f5413daa5db2ec906c402bec3bbfc7086
facelesshadow <aryanyadavsh369@gmail.com>
2024-01-01 21:21:09 +0530

    Added new files

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+int main() 
+{
+
+    cout<< "Hello, World!" << endl;
+    int a = 123;
+    cout<< a <<endl;
+
+
+
+    bool c = true;
+    cout<< c <<endl;
+    int size = sizeof(c);
+
+    cout<<"Size of a is: " << size <<endl;
+    return 1;
 }
