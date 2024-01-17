bb69c6544426ffdce247dd875a183f071222466c
facelesshadow <aryanyadavsh369@gmail.com>
2024-01-18 01:12:10 +0530

    Started studying conditionals

---
+++ conditionals.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /*int n;
+    cout << "Enter any number :" << endl;
+    cin >> n;
+    cout << "value of n is :" << n << endl;
+
+    if ( n > 1)
+    {
+        cout << "n is greater than 1" << endl;
+    }
+
+    int a = 2;
+    int b = a+1;
+
+    if ((a=3)==b) {
+        cout<<a;
+    }
+    else
+    {
+        cout<<a+1;
+    }*/
+
+    int a = 24;
+
+    if (a > 20)
+    {
+        cout<<"love";
+    }
+    else if(a==24)
+    {
+        cout<<"Hate";
+    }
+    else
+    {
+        cout<<"Babbar";
+    }
+    cout<<a;
+}
