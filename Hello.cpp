0a27cc4a9c58197216f6fc768e5e2d4809d18f01
facelesshadow <aryanyadavsh369@gmail.com>
2024-05-08 00:56:29 +0530

    Started DSA again

--- Hello.cpp
+++ Hello.cpp
@@ -2,6 +2,27 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+    /*cout << "Hello, World!" << endl;
+    char x, y;
+    cin >> x >> y;
+    cout << "value of x: " << x << endl << " Value of y: " << y;
     return 0;
+
+    int age;
+    cout << "Enter the age: ";
+    cin >> age;
+    if (age >= 18)
+    {
+        cout << "You are an adult";
+    }
+    else
+    {
+        cout << "You are a minor";
+    }*/
+
+    int marks;
+    cout << "Enter marks: ";
+    cin >> marks;
+    
+
 }
